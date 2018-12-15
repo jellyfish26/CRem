ffeca23385feffa379132dfc0b0bf43c8af34f87
fmacro <fmacro00@gmail.com>
2018-12-16 01:37:52 +0900

    Flex Message APIを問題リスト表示に適用, コメントかいた

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const test_accessor = require("./testlistaccessor");
 
-// 名称は後ですり合わせて変更する
+// 名称調整済み
 function question() {
     this.userId = "";
     this.question = "";
@@ -52,71 +52,135 @@ function handleEvent(event) {
         return Promise.resolve(null);
     }
 
-    var replyText = "";
+    // 返答に用いるmessage
+    var message = {
+        type: 'text',
+        text: "message"
+    };
 
     switch (state) {
         case BOT_STATE.NORMAL:
             //通常状態の動作の記述
-            replyText = normal_behaviour(event);
+            message = normal_behaviour(event);
             break;
 
         case BOT_STATE.QUESTION_WAITING:
             //問題入力待ちの動作の記述
-            replyText = question_waiting_behaviour(event);
+            message = question_waiting_behaviour(event);
             break;
 
         case BOT_STATE.ANSWER_WAITING:
             //回答入力待ちの動作の記述
-            replyText = answer_waiting_behaviour(event);
+            message = answer_waiting_behaviour(event);
             break;
 
         default:
-            replyText = "Error : 定義していないState";
             break;
     }
 
-    console.log(replyText);
+    console.log(message);
 
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: replyText //実際に返信の言葉を入れる箇所
-    });
+    return client.replyMessage(event.replyToken, message);
 }
 
 
 function normal_behaviour(event) {
+    // 問題追加トリガーが送られたとき問題待ちに移行
     if (event.message.text == "問題を追加") {
         state = BOT_STATE.QUESTION_WAITING;
-        return "問題文を入力してください";
+        var message = {
+            type: "text",
+            text: "問題文を入力してください"
+        };
+        return message;
     }
 
     if (event.message.text == "問題リストを表示") {
-        var question_list = "問題リスト:";
-        // 問題リストを取得，いい感じに並べる処理
         var listObj = test_accessor.getList(event.source.userId);
 
-        listObj.forEach(function(element){
-            question_list += "\n" + (element.question + "," + element.answer); 
-        });
-        
-        return question_list;
+        // リストの中身が存在するときはFlex Messageで返答を返す
+        if (listObj.length > 0) {
+            var listTemplate = require('./flexmessagetemplate');
+
+            listObj.forEach(function (element) {
+                question_list += "\n" + (element.question + "," + element.answer);
+                var list_part = {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": element.question,
+                            "size": "sm",
+                            "color": "#555555",
+                            "flex": 0
+                        },
+                        {
+                            "type": "text",
+                            "text": element.answer,
+                            "size": "sm",
+                            "color": "#111111",
+                            "align": "end"
+                        }
+                    ]
+                };
+
+                // 問題を入れるエリアに格納 -- 詳細はflexmessagetemplate.jsを参照
+                listTemplate.body.contents[2].contents.push(list_part);
+
+            });
+
+            // userIdを入れるエリアに格納 -- 詳細はflexmessagetemplate.jsを参照
+            listTemplate.body.contents[4].contents[1].text = event.source.userId;
+
+            // flex使うときの標準形
+            var message = {
+                "type": "flex",
+                "altText": "#",
+                "contents": {}
+            }
+
+            message.contents = listTemplate;
+
+            return message;
+        }
+        else
+        {
+            var message = {
+                "type": "text",
+                "text": "問題リストが空です"
+            };
+
+            return message;
+        }
     }
 
     if (event.message.text == "問題リストを消去") {
         // リスト消去処理
         test_accessor.clearList();
-        return "問題リストを消去しました";
+        var message = {
+            type: "text",
+            text: "問題リストを消去しました"
+        };
+        return message;
     }
 
     if (event.message.text == "説明文") {
         var app_description = "説明文";
-        return app_description;
+        var message = {
+            type: "text",
+            text: app_description
+        };
+        return message;
     }
 
 
     // 設定した文以外の文が送られてきたときの処理
-    var other_response = "ちがう文だよ";
-    return other_response;
+    var message = {
+        type: "text",
+        text: "未対応のメッセージです"
+    };
+    return message;
 
 }
 
@@ -128,7 +192,11 @@ function question_waiting_behaviour(event) {
     // 問題文を追加する処理 --------->
 
     state = BOT_STATE.ANSWER_WAITING;
-    return "回答を入力してください";
+    var message = {
+        type: "text",
+        text: "回答を入力してください"
+    };
+    return message;
 }
 
 function answer_waiting_behaviour(event) {
@@ -137,20 +205,25 @@ function answer_waiting_behaviour(event) {
     making_question.answer = event.message.text;
     making_question.userId = event.source.userId;
 
+    // テストのリストに格納
     test_accessor.addData(making_question);
 
     // 回答を追加する処理 ---------->
 
     var response
         = "(" + making_question.question
-        + "," + making_question.answer 
+        + "," + making_question.answer
         + ") で問題を登録しました";
-    
+
     making_question.question = "";
     making_question.answer = "";
 
     state = BOT_STATE.NORMAL;
-    return response;
+    var message = {
+        type: "text",
+        text: response
+    };
+    return message;
 }
 
 app.listen(PORT);
